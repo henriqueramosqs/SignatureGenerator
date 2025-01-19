3a9385e692a993aae8d92954da39489a1ce0d92a
Jhuzinha Santos <99686635+jhuzinha@users.noreply.github.com>
2025-01-19 19:29:09 -0300

    Add files via upload
    Essa é a parte dois do trabalho do algoritmo rsa da matéria de Segurança Computacional. 
    Descrição: 
    Parte II: Assinatura
    1. Cálculo de hashes da mensagem em claro (função de hash SHA-3)
    2. Assinatura da mensagem (cifração do hash da mensagem)
    3. Formatação do resultado (caracteres especiais e informações para verificação em
    BASE64)

---
+++ rsaParteII.py
@@ -0,0 +1,34 @@
+import hashlib
+import base64
+
+def calcular_hash(mensagem):
+    # biblioteca padrão do Python para hash, sha3_256() gera um valor de hash de 256 bits (32 bytes)
+    # digest() retorna o valor do hash calculado como uma sequência de bytes
+    return hashlib.sha3_256(mensagem.encode('utf-8')).digest()
+
+def cifrar_rsa(mensagem, chave_privada):
+    d, n = chave_privada
+    #from_bytes() converte uma sequência de bytes em um número inteiro.
+    # 'big' = a ordem dos bytes é da mais significativa para a menos significativa (big-endian)
+    mensagem_int = int.from_bytes(mensagem, 'big')
+    # pow é uma biblioteca de exponenciação modular onde pow(mensagem_int, d, n) = (mensagem_int^d)modn.
+    return pow(mensagem_int, d, n)
+
+def assinatura_mensagem(mensagem, chave_privada):
+    hash_mensagem = calcular_hash(mensagem)
+    assinatura = cifrar_rsa(hash_mensagem, chave_privada)
+    # adiciono uma margem de erro de 7 bits para ter certeza que terá espaço suficiente.
+    # faço uma divisão por 8 (pego o inteiro) para saber o nº de bytes.
+    tamanho_em_bytes = (assinatura.bit_length() + 7) // 8
+    assinatura_bytes = assinatura.to_bytes(tamanho_em_bytes, byteorder='big')
+    # base64.b64encode() recebe um valor em bytes e o codifica em base64.
+    # chr(b) converte o byte de volta para o caractere correspondente.
+    assinatura_base64 = ''.join([chr(b) for b in base64.b64encode(assinatura_bytes)]) 
+    return assinatura_base64
+
+if __name__ == "__main__":
+    mock_chave_privada = (123456789, 987654321987654321987654321987654321)
+    mensagem = "Testando assinatura"
+    assinatura = assinatura_mensagem(mensagem, mock_chave_privada)
+    print("Mensagem Original:", mensagem)
+    print("Assinatura (Base64):", assinatura)
